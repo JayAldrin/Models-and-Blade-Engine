572be6453216a7da84d61297d29370917820aa59
JayAldrin <j-p0371@students.uc-bcf.edu.ph>
2024-04-14 01:11:40 +0800

    edited the footer and header

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="flex justify-center items-center py-4">
-    <p>&copy; 2024 My Bookstore. All rights reserved.</p>
+    <p>2024 Aldrin's Bookstore</p>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,3 @@
 <header class="flex justify-center items-center py-4">
-    <h1 class="text-3xl font-bold">My Bookstore</h1>
+    <h1 class="text-3xl font-bold">Aldrin's Bookstore</h1>
 </header>
