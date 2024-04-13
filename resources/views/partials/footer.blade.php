5e734bc96d900a68a4af6dc5aef330d1dec32372
JayAldrin <j-p0371@students.uc-bcf.edu.ph>
2024-04-14 01:04:13 +0800

    initial commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="flex justify-center items-center py-4">
+    <p>&copy; 2024 My Bookstore. All rights reserved.</p>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,3 @@
+<header class="flex justify-center items-center py-4">
+    <h1 class="text-3xl font-bold">My Bookstore</h1>
+</header>
